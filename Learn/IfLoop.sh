558f7328b33268c98fc0eb993a707530c105aa72
Balaji <balajimail@zoho.com>
2022-12-24 07:24:03 +0530

    Women would desire

---
+++ Learn/IfLoop.sh
@@ -0,0 +1,20 @@
+read firstVal
+read secondVal
+
+#If condition is true
+if [ $firstVal -gt $secondVal ];
+then
+echo X is greater than Y
+fi
+
+#If condition is false
+if [ $firstVal -lt $secondVal ];
+then
+echo X is less than Y
+fi
+
+#If condition is same
+if [ $firstVal == $secondVal ];
+then
+echo X is equal to Y
+fi
